ee5b2b164b5ab283bd486a141d8b24aede4ed990
Roy Aringoy <roeder.aringoy@gmail.com>
2025-07-22 23:18:11 +0800

    Alter some data on sql

--- wallet-service/src/main/resources/data.sql
+++ wallet-service/src/main/resources/data.sql
@@ -7,4 +7,23 @@ CREATE TABLE IF NOT EXISTS wallets (
    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- TO RUN IN THE CONSOLE OF POSTGRES
+INSERT INTO wallets (id, user_id, balance, currency, status, last_updated)
+   SELECT '1d1b5bc8-f9e8-11ee-bb8b-0242ac120002',
+          '2d2b5bc8-f9e8-11ee-bb8b-0242ac120002',
+          12345.67,
+          'PHP',
+          'ACTIVE',
+          NOW()
+       WHERE NOT EXISTS (SELECT 1 FROM wallets WHERE id  = '1d1b5bc8-f9e8-11ee-bb8b-0242ac120002');
+
+INSERT INTO wallets (id, user_id, balance, currency, status, last_updated)
+    SELECT '3d3b5bc8-f9e8-11ee-bb8b-0242ac120002',
+           '4d4b5bc8-f9e8-11ee-bb8b-0242ac120002',
+           452.12,
+           'PHP',
+           'ACTIVE',
+           NOW()
+WHERE NOT EXISTS (SELECT 1 FROM wallets WHERE id  = '3d3b5bc8-f9e8-11ee-bb8b-0242ac120002');
+
 
