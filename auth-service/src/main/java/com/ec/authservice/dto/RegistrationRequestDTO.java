6633f838d9b4c066aceb6339f07f818dcaaa1759
Roy Aringoy <roeder.aringoy@gmail.com>
2025-07-30 12:11:47 +0800

    Add dto validation for auth service

--- auth-service/src/main/java/com/ec/authservice/dto/RegistrationRequestDTO.java
+++ auth-service/src/main/java/com/ec/authservice/dto/RegistrationRequestDTO.java
@@ -1,16 +1,39 @@
 package com.ec.authservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record RegistrationRequestDTO(
+        @Email(message = "Email should be valid")
+        @NotBlank(message = "Email is required")
         String email,
+
+        @Pattern(regexp="(^$|[0-9]{11})",message = "Mobile number must be 11 digits")
+        @NotBlank(message = "Phone No. is required")
         String phoneNumber,
+
+        @NotBlank(message = "Password is required")
         String password,
+
+        @NotBlank(message = "Role is required")
         String role,
+
+        @NotBlank(message = "First Name is required")
         String firstName,
+
+        @NotBlank(message = "Last Name is required")
         String lastName,
+
+        @NotBlank(message = "Address is required")
         String address,
+
+        @Past(message = "Date of  the birth must be in the past")
+        @NotBlank(message = "Date of Birth is required")
         LocalDate dateOfBirth
 ) {
 }
