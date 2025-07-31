d1e4b5fecc659a3288ca01b9bf7c100b2b0e4588
Roy Aringoy <roeder.aringoy@gmail.com>
2025-08-01 00:50:47 +0800

    Add password encoder dependency

--- auth-service/src/main/java/com/ec/authservice/service/UserService.java
+++ auth-service/src/main/java/com/ec/authservice/service/UserService.java
@@ -6,7 +6,9 @@ import com.ec.authservice.exception.PhoneNumberAlreadyExistsException;
 import com.ec.authservice.grpc.AccountServiceGrpcClient;
 import com.ec.authservice.model.User;
 import com.ec.authservice.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -15,17 +17,20 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final AccountServiceGrpcClient accountServiceGrpcClient;
+    private final PasswordEncoder passwordEncoder;
 
 
-    public UserService(UserRepository userRepository, AccountServiceGrpcClient accountServiceGrpcClient) {
+    public UserService(UserRepository userRepository, AccountServiceGrpcClient accountServiceGrpcClient, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.accountServiceGrpcClient = accountServiceGrpcClient;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<User> findByPhoneNumber(String phoneNo) {
         return userRepository.findByPhoneNumber(phoneNo);
     }
 
+    @Transactional
     public void register(RegistrationRequestDTO requestDTO) {
         if (userRepository.findByPhoneNumber(requestDTO.phoneNumber()).isPresent()) {
             throw new PhoneNumberAlreadyExistsException("Phone Number already exists");
@@ -38,7 +43,7 @@ public class UserService {
         User user = new User();
         user.setEmail(requestDTO.email());
         user.setPhoneNumber(requestDTO.phoneNumber());
-        user.setPassword(requestDTO.password());
+        user.setPassword(passwordEncoder.encode(requestDTO.password()));
         user.setRole(requestDTO.role());
 
         User newUser = userRepository.save(user);
