b401331b907d81d82017216abada19a899de5cb9
Roy Aringoy <roeder.aringoy@gmail.com>
2025-07-31 12:21:31 +0800

    Change expiry time from 10hrs to 1hr

--- auth-service/src/main/java/com/ec/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/ec/authservice/util/JwtUtil.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
-import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
@@ -30,7 +29,7 @@ public class JwtUtil {
                 .subject(email)
                 .claim("role", role)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hrs
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1HR
                 .signWith(secretKey)
                 .compact();
     }
